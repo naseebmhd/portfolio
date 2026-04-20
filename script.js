73e71cf423d80ac794ecc255b462f4df296054d2
naseebmhd <140338344+naseebmhd@users.noreply.github.com>
2026-04-20 22:34:27 +0530

    Improved UI, added counter, empty state, and better UX

--- script.js
+++ script.js
@@ -1,24 +1,80 @@
-window.addEventListener("scroll", () => {
-  document.querySelectorAll(".card").forEach(card => {
-    let position = card.getBoundingClientRect().top;
-    let screenHeight = window.innerHeight;
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-    if(position < screenHeight - 100){
-      card.style.opacity = 1;
-      card.style.transform = "translateY(0)";
-    }
-  });
-});
-const text = "Muhammad Naseeb";
-let i = 0;
-
-function typingEffect() {
-  if (i < text.length) {
-    document.getElementById("typing").innerHTML = text.substring(0, i + 1);
-    i++;
-    setTimeout(typingEffect, 100);
-  }
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-document.getElementById("typing").innerHTML = "";
-typingEffect();
+function addTask() {
+  let input = document.getElementById("taskInput");
+  if (input.value === "") return;
+
+if (input.value.length < 3) {
+  alert("Task must be at least 3 characters");
+  return;
+}
+
+  tasks.push({ text: input.value, completed: false });
+  input.value = "";
+  saveTasks();
+  displayTasks();
+}
+
+
+function displayTasks(filter = "all") {
+  let total = tasks.length;
+let completed = tasks.filter(t => t.completed).length;
+
+document.getElementById("taskCounter").innerText =
+  `Total: ${total} | Completed: ${completed}`;
+  let list = document.getElementById("taskList");
+  
+  list.innerHTML = "";
+
+if (tasks.length === 0) {
+  list.innerHTML = "<p>No tasks yet 🚀</p>";
+  return;
+}
+
+  tasks.forEach((task, index) => {
+    if (
+      (filter === "completed" && !task.completed) ||
+      (filter === "pending" && task.completed)
+    ) return;
+
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+      <span onclick="toggleTask(${index})" class="${task.completed ? 'completed' : ''}">
+        ${task.text}
+      </span>
+      <button onclick="deleteTask(${index})">X</button>
+    `;
+
+    list.appendChild(li);
+  });
+}
+
+function toggleTask(index) {
+  tasks[index].completed = !tasks[index].completed;
+  saveTasks();
+  displayTasks();
+}
+
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  saveTasks();
+  displayTasks();
+}
+
+function filterTasks(type) {
+  displayTasks(type);
+}
+
+// Load tasks on start
+displayTasks();
+
+document.getElementById("taskInput").addEventListener("keypress", function(e) {
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
